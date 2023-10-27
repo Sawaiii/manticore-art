48f6f71da087ae21d1f0504c167459322b2a9e3a
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2023-10-27 10:21:41 +0300

    Update HomeController.php

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -12,29 +12,40 @@ class HomeController extends Controller
 
 
     public function index(Request $request)
-    {
-        $catalogItems = [];
-        if (!empty($request->text)
-        ) {
-            $config = ['host' => 'manticore', 'port' => 9308];
-            $client = new Client($config);
-            $index = $client->index('prt_catalog');
+{
+    $catalogItems = [];
+    if (!empty($request->text)) {
+        $config = ['host' => 'manticore', 'port' => 9308];
+        $client = new Client($config);
+        $index = $client->index('prt_catalog');
 
-            $searchData =  $index->search($request->text)->limit(100)->orderBy('relevance', 'desc')->get();
+        $searchData = $index->search($request->text)->limit(100)->get();
 
-            $ids = [];
+        $ids = [];
 
-            foreach ($searchData as $searchDataItem) {
-                $ids[] = $searchDataItem->getId();
-            }
-
-            
-        $catalogItems = Catalog::whereIn("id" , $ids)->get();
-
-
-        // dd($catalogItems);
+        foreach ($searchData as $searchDataItem) {
+            $ids[] = $searchDataItem->getId();
         }
-        // return response()->json(["catalog" => $catalogItems] , 200);
-        return view('welcome',  [ "catalog" => $catalogItems] );
+
+        // Получить объекты Catalog
+        $catalogItems = Catalog::whereIn("id", $ids)->get();
+
+        // Сортировать результаты в PHP
+        usort($catalogItems, function($a, $b) use ($request) {
+    // Получите релевантность каждого элемента по введенному слову
+    $relevanceA = $a->getRelevance($request->text);
+    $relevanceB = $b->getRelevance($request->text);
+
+    // Сравните релевантность элементов
+    if ($relevanceA === $relevanceB) {
+        return 0;
     }
+
+    // Чем выше релевантность, тем ближе элемент к введенному слову
+    return ($relevanceA > $relevanceB) ? -1 : 1;
+});
+    }
+
+    return view('welcome', ["catalog" => $catalogItems]);
+}
 }
