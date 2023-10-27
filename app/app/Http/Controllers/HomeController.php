74e007f3ccca1afca66feb34562810f478551be0
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2023-10-27 10:24:13 +0300

    Update HomeController.php

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -12,40 +12,29 @@ class HomeController extends Controller
 
 
     public function index(Request $request)
-{
-    $catalogItems = [];
-    if (!empty($request->text)) {
-        $config = ['host' => 'manticore', 'port' => 9308];
-        $client = new Client($config);
-        $index = $client->index('prt_catalog');
+    {
+        $catalogItems = [];
+        if (!empty($request->text)
+        ) {
+            $config = ['host' => 'manticore', 'port' => 9308];
+            $client = new Client($config);
+            $index = $client->index('prt_catalog');
 
-        $searchData = $index->search($request->text)->limit(100)->get();
+            $searchData =  $index->search($request->text)->limit(100)->get();
 
-        $ids = [];
+            $ids = [];
 
-        foreach ($searchData as $searchDataItem) {
-            $ids[] = $searchDataItem->getId();
+            foreach ($searchData as $searchDataItem) {
+                $ids[] = $searchDataItem->getId();
+            }
+
+            
+        $catalogItems = Catalog::whereIn("id" , $ids)->get();
+
+
+        // dd($catalogItems);
         }
-
-        // Получить объекты Catalog
-        $catalogItems = Catalog::whereIn("id", $ids)->get();
-
-        // Сортировать результаты в PHP
-        usort($catalogItems, function($a, $b) use ($request) {
-    // Получите релевантность каждого элемента по введенному слову
-    $relevanceA = $a->getRelevance($request->text);
-    $relevanceB = $b->getRelevance($request->text);
-
-    // Сравните релевантность элементов
-    if ($relevanceA === $relevanceB) {
-        return 0;
+        // return response()->json(["catalog" => $catalogItems] , 200);
+        return view('welcome',  [ "catalog" => $catalogItems] );
     }
-
-    // Чем выше релевантность, тем ближе элемент к введенному слову
-    return ($relevanceA > $relevanceB) ? -1 : 1;
-});
-    }
-
-    return view('welcome', ["catalog" => $catalogItems]);
-}
 }
