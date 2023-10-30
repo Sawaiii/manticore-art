040ea922dce53ccb58ab443ac312d5edce38a401
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2023-10-30 10:31:45 +0300

    Update HomeController.php

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
             $client = new Client($config);
             $index = $client->index('prt_catalog');
 
-            $searchData =  $index->search($request->text)->rsort('Name','desc')->limit(100)->get();
+            $searchData =  $index->search($request->text)->sort('Name','desc')->limit(100)->get();
 
             $ids = [];
 
