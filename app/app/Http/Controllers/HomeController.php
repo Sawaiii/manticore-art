f502a9a99b48771442aa0c20a4624495a679cffb
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2023-10-30 12:32:11 +0300

    Update HomeController.php

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -12,38 +12,29 @@ class HomeController extends Controller
 
 
     public function index(Request $request)
-{
-    $catalogItems = [];
+    {
+        $catalogItems = [];
+        if (!empty($request->text)
+        ) {
+            $config = ['host' => 'manticore', 'port' => 9308];
+            $client = new Client($config);
+            $index = $client->index('prt_catalog');
 
-    if (!empty($request->text)) {
-        $config = ['host' => 'manticore', 'port' => 9308];
-        $client = new Client($config);
-        $index = $client->index('prt_catalog');
+            $searchData =  $index->search($request->text)->limit(100)->get();
 
-        $searchData = $index->search($request->text)->limit(100)->get();
-        $ids = [];
+            $ids = [];
 
-        foreach ($searchData as $searchDataItem) {
-            $ids[] = $searchDataItem->getId();
-        }
-
-        $catalogItems = Catalog::whereIn("id", $ids)->get();
-
-        // Сортировка на основе соответствия первому слову
-        $catalogItems = $catalogItems->sort(function ($a, $b) use ($request) {
-            $aFirstWord = strtok($a->Name, ' '); // Замените 'your_text_column' на имя столбца, содержащего текст
-            $bFirstWord = strtok($b->Name, ' ');
-
-            if (strpos($aFirstWord, $request->text) === 0 && strpos($bFirstWord, $request->text) !== 0) {
-                return -1;
-            } elseif (strpos($aFirstWord, $request->text) !== 0 && strpos($bFirstWord, $request->text) === 0) {
-                return 1;
+            foreach ($searchData as $searchDataItem) {
+                $ids[] = $searchDataItem->getId();
             }
 
-            return 0;
-        })->values()->all();
-    }
+            
+        $catalogItems = Catalog::whereIn("id" , $ids)->get();
 
-    return view('welcome',  [ "catalog" => $catalogItems] );
-}
+
+        // dd($catalogItems);
+        }
+        // return response()->json(["catalog" => $catalogItems] , 200);
+        return view('welcome',  [ "catalog" => $catalogItems] );
+    }
 }
