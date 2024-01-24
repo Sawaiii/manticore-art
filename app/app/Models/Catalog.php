81f0e8f46c30cfdfbeee3ae7af52e84c0cd1aabe
Sawaii <133211004+Sawaiii@users.noreply.github.com>
2024-01-24 10:12:02 +0300

    Update Catalog.php

--- app/app/Models/Catalog.php
+++ app/app/Models/Catalog.php
@@ -16,6 +16,6 @@ class Catalog extends Model
 
     public function razdel():BelongsTo
     {
-        return $this->belongsTo(Razdel::class , 'Razdel');
+        return $this->belongsTo(Razdel::class , 'Razdel', 'ID');
     }
 }
